e07c4b8a45b84506f88411a3361eb68d09ad9ec3
khush157 <159526966+khush157@users.noreply.github.com>
2025-11-13 13:17:32 +0530

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,7 @@
+// Elements
+const form        = document.getElementById('expense-form');
+const titleInput  = document.getElementById('title');
+const amountInput = document.getElementById('amount');
+const categoryIn  = document.getElementById('category');
+const dateInput   = document.getElementById('date');
+const listEl      = document.getElementById('expense-list');
